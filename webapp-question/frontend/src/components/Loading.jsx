47906140ce8b26ba5a831c92f73ea1b47696a886
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 02:02:16 +0700

    testing modal

--- webapp-question/frontend/src/components/Loading.jsx
+++ webapp-question/frontend/src/components/Loading.jsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen bg-white">
-      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce animate-delay-300"></div>
-      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce animate-delay-150"></div>
+    <div className="flex space-x-2 items-center justify-center h-screen bg-white">
+      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
       <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce"></div>
     </div>
   );
