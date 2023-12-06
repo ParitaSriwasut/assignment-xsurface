8ae7f58eadd61eb605899fc49edc6b21edc9a472
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 01:47:10 +0700

    test

--- webapp-question/frontend/src/components/Loading.jsx
+++ webapp-question/frontend/src/components/Loading.jsx
@@ -3,6 +3,7 @@ export default function Loading() {
     <div className="flex space-x-2 justify-center items-center bg-secondary-500 h-screen dark:invert">
       <span className="sr-only">Loading...</span>
       <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce delay-[animation-delay:-0.3s]"></div>
+      <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce delay-[animation-delay:-0.15s]"></div>
       <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce"></div>
     </div>
   );
