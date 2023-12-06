f0dd4447d36e9504c8daa428aea9cc5338a7d395
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 01:57:31 +0700

    commiting befor adding product details modal

--- webapp-question/frontend/src/components/Loading.jsx
+++ webapp-question/frontend/src/components/Loading.jsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="rounded-full h-20 w-20 bg-primary-500 animate-ping"></div>
+    <div className="flex items-center justify-center h-screen bg-white">
+      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce animate-delay-300"></div>
+      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce animate-delay-150"></div>
+      <div className="h-8 w-8 bg-primary-500 rounded-full animate-bounce"></div>
     </div>
   );
 }
