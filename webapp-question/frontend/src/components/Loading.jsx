28f09e46293c342c49c613862c7952191a1fe2c5
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 01:50:20 +0700

    spinner changed

--- webapp-question/frontend/src/components/Loading.jsx
+++ webapp-question/frontend/src/components/Loading.jsx
@@ -1,10 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex space-x-2 justify-center items-center bg-secondary-500 h-screen dark:invert">
-      <span className="sr-only">Loading...</span>
-      <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce delay-[animation-delay:-0.3s]"></div>
-      <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce delay-[animation-delay:-0.15s]"></div>
-      <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce"></div>
+    <div className="flex justify-center items-center h-screen">
+      <div className="rounded-full h-20 w-20 bg-primary-500 animate-ping"></div>
     </div>
   );
 }
