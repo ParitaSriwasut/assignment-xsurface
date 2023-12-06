6faeafebf890ec792bf033bea63894775dd6255b
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 01:45:36 +0700

    spinner fixed

--- webapp-question/frontend/src/components/Loading.jsx
+++ webapp-question/frontend/src/components/Loading.jsx
@@ -1,10 +1,9 @@
 export default function Loading() {
   return (
-    <div className="flex space-x-2 justify-center items-center bg-white h-screen dark:invert">
+    <div className="flex space-x-2 justify-center items-center bg-secondary-500 h-screen dark:invert">
       <span className="sr-only">Loading...</span>
-      <div className="h-8 w-8 bg-secondary-500 rounded-full animate-bounce delay-[animation-delay:-0.3s]"></div>
-      <div className="h-8 w-8 bg-secondary-500 rounded-full animate-bounce delay-[animation-delay:-0.15s]"></div>
-      <div className="h-8 w-8 bg-secondary-500 rounded-full animate-bounce"></div>
+      <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce delay-[animation-delay:-0.3s]"></div>
+      <div className="h-8 w-8 bg-secondary-white rounded-full animate-bounce"></div>
     </div>
   );
 }
