b028329be03374ad33aa600081b107a6fcf31939
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 01:03:42 +0700

    search input

--- webapp-question/frontend/src/components/product/ProductList.jsx
+++ webapp-question/frontend/src/components/product/ProductList.jsx
@@ -15,7 +15,7 @@ export default function ProductList({ products }) {
               className="w-70 bg-white shadow-lg rounded-xl duration-500 hover:scale-105 hover:shadow-xl mx-2 my-4" // Added mx-2 and my-4 for margin
             >
               <img
-                className="h-72 w-72 object-cover rounded-t-xl"
+                className="h-72 w-80 object-cover rounded-t-xl"
                 src={product.image}
                 alt={product.name}
               />
@@ -26,10 +26,10 @@ export default function ProductList({ products }) {
                 <p className="text-secondary-400 text-md font-semibold cursor-auto my-3">
                   {product.code}
                 </p>
-                <p className="text-primary-700 text-lg font-semibold cursor-auto ml-3">
+                <p className="text-primary-700 text-lg font-semibold cursor-auto my-3">
                   ฿ {product.price}
                 </p>
-                <div className="ml-auto">
+                <div className="mr-auto">
                   <FaCircleInfo
                     className="flex text-2xl font-bold text-secondary-400 cursor-pointer duration-500 hover:scale-105"
                     onClick={() => {
--- webapp-question/frontend/src/components/product/SearchInput.jsx
+++ webapp-question/frontend/src/components/product/SearchInput.jsx
@@ -4,17 +4,17 @@ export default function SearchInput({ handleSearchInputChange, searchInput }) {
   return (
     <section className="bg-white">
       <div className="flex items-center justify-center pt-6 px-12 mx-auto">
-        <div className="relative">
+        <div className="relative flex items-center">
+          <div className="absolute left-3 inset-y-0 flex items-center">
+            <GrSearch className="flex text-2xl font-bold text-secondary-400" />
+          </div>
           <input
-            className="appearance-none border-2 border-secondary-400 hover:border-secondary-200 transition-colors rounded-2xl w-[800px] py-2 px-4 text-secondary-500 leading-tight focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:shadow-outline"
+            className="appearance-none border-2 border-secondary-400 hover:border-secondary-200 transition-colors rounded-2xl w-[800px] py-2 pl-10 pr-4 text-secondary-500 leading-tight focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:shadow-outline"
             type="text"
             placeholder="Name, Catalogue, Code"
             onChange={handleSearchInputChange}
             value={searchInput}
           />
-          <div className="absolute left-3 inset-y-0 flex items-center">
-            <GrSearch className="flex text-2xl font-bold text-secondary-400" />
-          </div>
         </div>
       </div>
     </section>
