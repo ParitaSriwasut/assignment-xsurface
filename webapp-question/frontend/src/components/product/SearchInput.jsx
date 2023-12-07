035b02431a1525164d51dbd19a87be3dbb6868e3
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 22:48:02 +0700

    search input

--- webapp-question/frontend/src/components/product/SearchInput.jsx
+++ webapp-question/frontend/src/components/product/SearchInput.jsx
@@ -1,52 +1,22 @@
 import { GrSearch } from "react-icons/gr";
-import { RiArrowDropDownLine } from "react-icons/ri";
 
 export default function SearchInput({ handleSearchInputChange, searchInput }) {
   return (
-    <form>
-      <div className="sm:w-640 md:w-768 lg:w-1024 xl:w-1280 2xl:w-1536 pt-16 px-12 mx-auto flex items-center justify-center">
-        <button
-          className="z-10 inline-flex flex-shrink-0 items-center rounded-s-lg border border-secondary-100 bg-white px-4 py-2.5 text-center text-sm font-medium text-secondary-500 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-400"
-          type="button"
-        >
-          Search by
-          <RiArrowDropDownLine className="text-secondary-500 flex text-2xl font-bold" />
-        </button>
-        <div className="z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow">
-          <ul className="py-2 text-sm text-secondary-500">
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-secondary-100 "
-              >
-                Product name
-              </button>
-            </li>
-            <li>
-              <button
-                type="button"
-                className="inline-flex w-full px-4 py-2 hover:bg-secondary-100 "
-              >
-                Product code
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div className="relative w-full">
+    <section className="bg-white">
+      <div className="sm:w-640 md:w-768 lg:w-1024 xl:w-1280 2xl:w-1536 mx-auto flex items-center justify-center pt-6 px-12 ">
+        <div className="relative flex items-center">
+          <div className="absolute left-3 inset-y-0 flex items-center">
+            <GrSearch className="flex text-2xl font-bold text-secondary-400" />
+          </div>
           <input
-            type="search"
-            className="z-20 block w-full rounded-e-lg border-2 p-2.5 text-sm text-secondary-500 border-secondary-100 mt-1 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:border-transparent"
-            placeholder="Name, Code "
-            required
+            className="appearance-none border-2 border-secondary-400 hover:border-secondary-200 transition-colors rounded-2xl w-[700px] py-2 pl-10 pr-4 text-secondary-500 leading-tight focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:shadow-outline"
+            type="text"
+            placeholder="Name, Catalogue, Code"
+            onChange={handleSearchInputChange}
+            value={searchInput}
           />
-          <button
-            type="submit"
-            className="absolute end-0 top-0 h-full rounded-e-lg border border-primary-500 bg-primary-500 p-2.5 text-md font-bold text-white hover:bg-primary-400"
-          >
-            <GrSearch className="flex " />
-          </button>
         </div>
       </div>
-    </form>
+    </section>
   );
 }
