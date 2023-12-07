0efee6caf80e6402501f89b564454bc2c87be284
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-07 23:35:07 +0700

    response fixed

--- webapp-question/backend/routes/router.js
+++ webapp-question/backend/routes/router.js
@@ -1,12 +1,16 @@
-import { Router } from 'express';
-import { searchProduct, createProduct } from '../controllers/product_controller.js';
-import upload from '../middlewares/upload.js';
+import { Router } from "express";
+import {
+  searchProduct,
+  createProduct,
+} from "../controllers/product_controller.js";
+import upload from "../middlewares/upload.js";
 
 const router = Router();
 
-router.get('/', (req, res) => {res.send('Hello World!')});
-router.get('/products', searchProduct);
-router.post('/products', upload.single("file"), createProduct);
+router.get("/", (req, res) => {
+  res.send("Hello xSurface!");
+});
+router.get("/products", searchProduct);
+router.post("/products", upload.single("file"), createProduct);
 
-// Export the router
 export default router;
