92b14c9283485dc9a60b84b37658614bd7d5647d
ParitaSriwasut <parita.sri0327@gmail.com>
2023-12-12 16:00:14 +0700

    search input component is done

--- webapp-question/frontend/src/components/product/SearchInput.jsx
+++ webapp-question/frontend/src/components/product/SearchInput.jsx
@@ -3,10 +3,10 @@ import { GrSearch } from "react-icons/gr";
 export default function SearchInput({ handleSearchInputChange, searchInput }) {
   return (
     <div className="sm:w-640 md:w-768 lg:w-1024 xl:w-1280 2xl:w-1536 mx-auto flex items-center justify-center pt-6 px-12 bg-white">
-      <div className="relative left-6 top-2 inset-y-0 flex items-center">
-        <GrSearch className="absolute flex text-2xl font-bold text-secondary-400" />
+      <div className="relative top-2 inset-y-0 flex items-center w-[80%]">
+        <GrSearch className="absolute left-3 flex text-2xl font-bold text-secondary-400" />
         <input
-          className="appearance-none border-2 border-secondary-400 hover:border-secondary-200 transition-colors rounded-2xl w-[640px] py-2 pl-10 pr-4 text-secondary-500 leading-tight focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:shadow-outline"
+          className="appearance-none border-2 border-secondary-400 hover:border-secondary-200 w-full transition-colors rounded-2xl py-2 pl-10 pr-4 text-secondary-500 leading-tight focus:outline-none focus:ring-primary-500 focus:border-primary-500 focus:shadow-outline"
           type="text"
           placeholder="Search by Product name, Product code"
           onChange={handleSearchInputChange}
--- webapp-question/frontend/src/router/Router.jsx
+++ webapp-question/frontend/src/router/Router.jsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import CreateProductPage from "../pages/CreateProductPage";
+import SearchInput from "../components/product/SearchInput";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/create", element: <CreateProductPage /> },
+  { path: "/search", element: <SearchInput /> },
 ]);
 
 export default function Router() {
